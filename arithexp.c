a4a8760119a542dcaa64726371c1a2fae527bd25
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-25 21:02:23 +0530

    Create arithexp.c

---
+++ arithexp.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a=40,b=20,add,sub,mul,div,mod;
+  add=a+b;
+  sub=a-b;
+  mul=a*b;
+  div=a/b;
+  mod=a%b;
+  printf("addition of a,b is: %d\n",add);
+  printf("substraction of a,b is: %d\n",sub);
+  printf("multiplication of a,b is: %d\n",mul);
+  printf("division of a,b is: %d\n",div);
+  printf("modulus of a,b is: %d\n",mod);
+  return 0;
+}
+  
+        
