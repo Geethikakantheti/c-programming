c5387896778d739946b0c9eb904a3cafe0690de3
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 16:52:24 +0530

    Add files via upload

---
+++ INCH.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int l,b,area,per;
+ /*printf("enterl,b values:\n");*/
+ /*scanf("%d%d",&l,&b);*/
+ area=l*b;
+ per=2*(l+b);
+ printf("area is:%d",area);
+ printf("perimeter is:%d",per);
+ }
