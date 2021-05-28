672577cb6ad6f5b32194c6b79dbe1aa9e11b4b0e
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 16:43:00 +0530

    Add files via upload

---
+++ LINEDEMO.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int input;
+ printf("please enter the input:\n");
+ scanf("%d",& input);
+ printf("the input value is:\n");
+ printf("%d",input);
+ return 0;
+ }
+
