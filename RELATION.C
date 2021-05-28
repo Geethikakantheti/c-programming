a6474fccae933995acdee2675613610308160c6b
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 16:23:54 +0530

    Add files via upload

---
+++ RELATION.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int x,y;
+ double z;
+ x=7;
+ y=25;
+ z=24,46;
+ printf("given x=%d,y=%d,andz=%2f,\n",x,y,z);
+ printf("x>=y produces :%d\n",x>=y);
+ printf("x==y produces :%d\n",x==y);
+ printf("x<z  produces :%d\n",x<y);
+ printf("y>z  produces :%d\n",y>z);
+ printf("x!=y-18 produces :%d\n",x!=y-18);
+ printf("x+y!=z produces:%d\n",x+y!=2);
+ return 0;
+ }
---
+++ TABDEMO.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ char ch='A';
+ char str[20]="welcome to c";
+ float flt=10.234;
+ int no=150;
+ double dbl=20.123456;
+ printf("character is %c \n",ch);
+ printf("string is %s \n",str);
+ printf("float value is %f \n",flt);
+ printf("integer value is %d \n",no);
+ printf("double value is %ld \n",dbl);
+ printf("octal value is %o \n",no);
+ printf("hexa decimal value is %x \n",no);
+ return 0;
+ }
